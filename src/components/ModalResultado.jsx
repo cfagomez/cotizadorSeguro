37657232595f8beaab64b20d68795919ba41f4b8
Federico <cuervog992@gmail.com>
2023-11-07 22:18:25 -0300

    estilos modal

--- src/components/ModalResultado.jsx
+++ src/components/ModalResultado.jsx
@@ -3,14 +3,32 @@ import useCotizador from '../hooks/useCotizador'
 
 const ModalResultado = () => {
 
-    const {reestablecerDatos} = useCotizador()
+    const {resultado, reestablecerDatos, datos} = useCotizador()
+    const {marca, plan, año} = datos
 
   return (
     <div className='modal'>
       <div className='modal-content'>
-        <p>Resultado</p>
+        <div className='div-total m-bottom'>
+            <h2>Total: </h2>
+            <p className='p-total'>{resultado}</p>
+        </div>
+        <div className='flex-div'>
+            <h3>Marca: </h3>
+            <span>{marca}</span>
+        </div>
+        <hr />
+        <div className='flex-div'>
+            <h3>Año: </h3>
+            <span>{año}</span>
+        </div>
+        <hr />
+        <div className='flex-div'>
+            <h3>Plan: </h3>
+            <span>{plan}</span>
+        </div>
         <button
-            className='button'
+            className='button m-top'
             onClick={reestablecerDatos}
         >
             Aceptar
